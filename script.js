4d925448a4842050e5a32192f82aa2d941953a77
Mark <markwilkins86@gmail.com>
2022-04-11 11:32:03 -0400

    add style.css, add script.js, connect to index.html

--- script.js
+++ script.js
@@ -3,7 +3,3 @@ h2.textContent = "Welcome to Made of Mark";
 
 document.querySelector("body").appendChild(h2);
 
-const h3 = document.createElement("h3");
-h3.textContent = "About Me";
-
-document.querySelectoer("body").appendChild(h3);
