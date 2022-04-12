57edbd78f10bc2e7aee2f6019564321038ed5ca9
Mark <markwilkins86@gmail.com>
2022-04-12 14:29:33 -0400

    add script.js, add style.css, connect index.html

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ h2.textContent = "Welcome to Made of Mark";
 
 document.querySelector("body").appendChild(h2);
 
+function changeColor(color) {
+    document.querySelector(".box").style.color = color;
+}
